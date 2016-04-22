f699734fc4fc19df3a8a38f042dbf46e9a733a26
Timothy Mueller <TJMueller@DonorSnap.com>
2016-04-22 19:49:33 -0400

    CLC-48 CLC-25 Started coding view as well as made a view model

---
+++ CircularLogic/CircularLogic.UI/Models/AdminIndexVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CircularLogic.Models;
+
+namespace CircularLogic.UI.Models
+{
+    public class AdminIndexVM
+    {
+        public List<Category> Categories { get; set; }
+        public List<BlogPost> BlogPosts { get; set; }
+        public List<ApplicationUser> Users { get; set; }
+    }
+}
