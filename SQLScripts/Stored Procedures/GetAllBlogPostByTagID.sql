e9d50b5e466f8e788a6f5fb2bcabf7b3e3bd0417
Tony Maiorana <tmaiorana23@gmail.com>
2016-04-24 14:39:40 -0400

    CLC-49 added SPROCs for Get Alls and GetBlogPost by(whatever)

---
+++ SQLScripts/Stored Procedures/GetAllBlogPostByTagID.sql
@@ -0,0 +1,40 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Tony Maiorana>
+-- Create date: <Create Date,,4/24/2016>
+-- Description:	<Description,,Get All BlogPost By TagID>
+-- =============================================
+CREATE PROCEDURE GetAllBlogPostByTagID
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+SELECT c.CategoryName, bp.Title, bp.TextBody, bp.PostTime, t.TagName
+ FROM BlogPost bp
+INNER JOIN Category c
+ ON c.CategoryID = bp.CategoryID
+INNER JOIN PostTag pt
+ ON pt.BlogPostID = bp.BlogPostID
+LEFT JOIN Tag t
+ ON t.TagID = pt.TagID
+WHERE t.tagID = pt.TagID
+END
+GO
