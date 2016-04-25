3485a5e76ec8ac2e3cefe767e10dd5cadd7b09e3
Timothy Mueller <TJMueller@DonorSnap.com>
2016-04-25 10:20:48 -0400

    CLC-48 CLC-25 allow deny check boxes can now post to api. after successful post they are removed.

--- CircularLogic/CircularLogic.UI/Controllers/AdminAPIController.cs
+++ CircularLogic/CircularLogic.UI/Controllers/AdminAPIController.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CircularLogic.UI.Models;
 
 namespace CircularLogic.UI.Controllers
 {
     public class AdminAPIController : ApiController
     {
+        [HttpPost]
+        public HttpResponseMessage Post(AllowDeny ad)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.Accepted);
+            return response;
+        }
     }
 }
