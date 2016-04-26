88b73519a77db6418d49bdce23f373d54300e419
Timothy Mueller <TJMueller@DonorSnap.com>
2016-04-26 11:25:25 -0400

    sql

--- SQLScripts/Stored Procedures/GetAdminQueue.sql
+++ SQLScripts/Stored Procedures/GetAdminQueue.sql
@@ -1,24 +1,19 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
+USE [CircularLogic]
+GO
+
+/****** Object:  StoredProcedure [dbo].[GetAdminQueue]    Script Date: 4/26/2016 11:16:00 AM ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE PROCEDURE GetAdminQueue 
+CREATE PROCEDURE [dbo].[GetAdminQueue] 
 	-- Add the parameters for the stored procedure here
 
 AS
@@ -30,7 +25,10 @@ BEGIN
     -- Insert statements for procedure here
 	SELECT * FROM BlogPost bp
 		INNER JOIN Category c ON c.CategoryID = bp.CategoryID
-		LEFT JOIN ContentQueue q ON bp.BlogPostID = q.NewBlogPostID
+		INNER JOIN ContentQueue q ON bp.BlogPostID = q.NewBlogPostID
 		WHERE bp.IsApproved = 0 AND bp.IsDeleted = 0
 END
+
 GO
+
+
