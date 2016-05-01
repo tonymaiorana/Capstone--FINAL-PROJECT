4e3d630986cf7ee6a6206fb04e2981836963d555
William R Poulson <wrpoulson@gmail.com>
2016-05-01 02:41:10 -0400

    CLC-9 sidebars added to both sides

---
+++ CircularLogic/CircularLogic.UI/Scripts/app/sidebars.js
@@ -0,0 +1,11 @@
+﻿
+/*Menu-toggle*/
+$("#menu-toggle-left").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("active-left");
+});
+
+$("#menu-toggle-right").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("active-right");
+});
