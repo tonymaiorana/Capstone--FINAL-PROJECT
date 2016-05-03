f881f520af64fd8453f3bc1446acb59c17324c46
William R Poulson <wrpoulson@gmail.com>
2016-05-03 15:44:43 -0400

    CLC-8 custom routing is done and working on layout

--- CircularLogic/CircularLogic.UI/App_Start/RouteConfig.cs
+++ CircularLogic/CircularLogic.UI/App_Start/RouteConfig.cs
@@ -13,18 +13,79 @@ namespace CircularLogic.UI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
             routes.MapRoute(
-               name: "BillSucks",
-               url: "BillSucks",
-               defaults: new { controller = "BlogPost", action = "BlogHomePage", id = UrlParameter.Optional }
+               name: "Create Blog Post",
+               url: "Blog/Create/{id}",
+               defaults: new { controller = "BlogPost", action = "CreateBlogPost", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "About Circular Logic",
+               url: "About",
+               defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Contact Us",
+               url: "Contact",
+               defaults: new { controller = "Home", action = "Contact", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Dev's Round Table",
+               url: "Blog",
+               defaults: new { controller = "BlogPost", action = "Blogs", id = UrlParameter.Optional }
            );
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+               name: "Blog Post",
+               url: "Blog/Post/{id}",
+               defaults: new { controller = "BlogPost", action = "BlogPostDetail", id = UrlParameter.Optional }
             );
-           
+
+            routes.MapRoute(
+               name: "Edit Blog Post",
+               url: "Blog/Edit/{id}",
+               defaults: new { controller = "BlogPost", action = "EditBlog", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Create New Page",
+               url: "CreatePage",
+               defaults: new { controller = "StaticPage", action = "AddStaticPage", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Delete Page",
+               url: "DeletePage",
+               defaults: new { controller = "StaticPage", action = "DeleteStaticPage", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Edit Pages",
+               url: "EditPages",
+               defaults: new { controller = "StaticPage", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Update Page",
+               url: "CreatePage",
+               defaults: new { controller = "StaticPage", action = "AddStaticPage", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Page",
+               url: "Page/{id}",
+               defaults: new { controller = "StaticPage", action = "ViewStaticPage", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+               name: "Circular Logic",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
         }
     }
 }
