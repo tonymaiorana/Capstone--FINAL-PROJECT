f6abbb98265ddb2959472ba64bd41b014b52a366
Timothy Mueller <TJMueller@DonorSnap.com>
2016-05-03 13:24:10 -0400

    stuff

--- CircularLogic/CircularLogic.UI/App_Start/RouteConfig.cs
+++ CircularLogic/CircularLogic.UI/App_Start/RouteConfig.cs
@@ -14,10 +14,17 @@ namespace CircularLogic.UI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "BillSucks",
+               url: "BillSucks",
+               defaults: new { controller = "BlogPost", action = "BlogHomePage", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+           
         }
     }
 }
