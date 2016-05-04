118dc3b3162fe798df729420e8b6df3896be9776
Timothy Mueller <TJMueller@DonorSnap.com>
2016-05-04 16:06:08 -0400

    fdg

--- CircularLogic/CircularLogic.UI/Scripts/app/BlogSearchApp.js
+++ CircularLogic/CircularLogic.UI/Scripts/app/BlogSearchApp.js
@@ -1,25 +1,9 @@
 ﻿var app = angular.module('angularTable', ['angularUtils.directives.dirPagination']);
 
-//app.config(function ($routeProvider) {
-//    $routeProvider
-//        .when('/blogs', {
-//            controller: 'listdata',
-//            templateUrl: '/AngularViews/BlogSearchList.html'
-//        })
-//        .otherwise({ redirectTo: '/blogs' });
-//});
-
-
 app.controller('listdata',function($scope, $http){
 	$scope.posts = []; //declare an empty array
 	$http.get("/api/BlogPostAPI/GetPreviews").success(function(response){ 
 	    $scope.posts = response;  //ajax request to fetch data into $scope.data
-	    $scope.Categories = [];
-	    angular.forEach(response, function(item) {
-	        if ($scope.Categories.indexOf(item.Category) === -1) {
-	            $scope.Categories.push(item.Category);
-	        }
-	    });
 	    $scope.sort('DateTicks');
 	});
 
